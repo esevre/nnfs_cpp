fa0cda157ae84d71ece13cfec94536c15d00c6be
esevre <esevre@gmail.com>
2021-01-07 20:40:13 +0900

    Added a couple methods to assist working with vectors.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
-int main() {
+#include <algorithm>
+#include <vector>
+// simple linspace method
+// todo: update later with more abstraction
+std::vector<double> linspace(double first, double last, int size)
+{
+    std::vector<double> vector(size);
+    std::generate(vector.begin(),
+            vector.end(),
+            [i=0, first, dx=(last-first)/(size-1.0)]()mutable->double{ return first + dx*i++;});
+    return vector;
+}
 
+// Simple container printer
+template <class Container>
+void print(const Container &container)
+{
+    for (const auto &elt : container) {
+        std::cout << elt << ", ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    auto x = linspace(0, 10, 21);
+    print(x);
 }
